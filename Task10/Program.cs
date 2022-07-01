9909ce33d65dec28c21779a1a0bb33e98805e5f0
Kseniya <ksavierra@rambler.ru>
2022-07-01 20:05:03 +0500

    Решение задачи 10

---
+++ Task10/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("three-digit number: ");
+string number = Console.ReadLine();
+Console.WriteLine(number[1]);
