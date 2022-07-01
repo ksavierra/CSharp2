7cf4a16703be9582578d13f1d816d61744095147
Kseniya <ksavierra@rambler.ru>
2022-07-01 20:05:53 +0500

    Решение задачи 15

---
+++ Task15/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день
+
+Console.WriteLine("Укажите номер дня недели");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if(day >= 6)
+    if (day >=8)
+        Console.WriteLine("Неделя состоит из 7 дней");
+    else 
+        Console.WriteLine("выходной");
+else if (day <= 0)
+Console.WriteLine("Неделя начниается с первого дня");
+else if (day <= 5)
+    Console.WriteLine("будни");
